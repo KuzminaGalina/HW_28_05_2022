cdd6ef4db67e4e1a0be39dfcaf3858e7f55a62c5
Galina <kuzmina0497@mail.ru>
2022-05-28 21:43:57 +0300

    Добавлен алгоритм

---
+++ Unit_2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+
+int SummNumber(int number)
+{
+    int summ = 0;
+    while (number > 0)
+    {
+    summ = summ + number % 10;
+    number = number / 10;
+    }
+return summ;
+}
+Console.WriteLine("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = SummNumber(number);
+Console.WriteLine($"Сумма цифр числа {number} равна: {result}");
+
